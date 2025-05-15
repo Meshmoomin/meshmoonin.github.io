42c0a620c0040bf15c9ac1276a8088adf2bcb49b
1lux <anton.lux@studium.uni-hamburg.de>
2025-05-15 19:21:42 +0200

    didnt fix anything

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,7 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// Enable web support
-config.resolver.assetExts.push("web.js");
-config.resolver.sourceExts.push("web.js");
 
 module.exports = config;
